f66cb8214896dc8caf0cd62b46af8d605571fcd6
Zhixiang Wang <alexw@Zhixiangs-MacBook-Air.local>
2019-12-02 00:38:58 -0600

    Added brush

--- Source/project.js
+++ Source/project.js
@@ -71,6 +71,7 @@ function plot_it()  {
 		.attr('id', 'filter')
 		.attr('fill', 'gray')
 		.attr('transform', 'translate(25, 300)')
+
 	// Threshold line
 	d3.select('svg').append('line').attr('id', 'threshold')
 		.attr('x1', 0).attr('x2', 30).attr('y1', filter_height).attr('y2', filter_height)
@@ -82,17 +83,17 @@ function plot_it()  {
 	d3.select('svg')
 		.append('g')
 		.attr('transform', 'translate('+left_pad+','+y_pad+')')
-		.attr('id', 'pose')
+		.attr('id', 'pose');
 
 	d3.select('svg')
 		.append('g')
 		.attr('transform', 'translate('+left_pad+','+(height + 100)+')')
-		.attr('id', 'righthand')
+		.attr('id', 'lefthand');
 
 	d3.select('svg')
 		.append('g')
 		.attr('transform', 'translate('+left_pad+','+ (2 * (height + 100))+')')
-		.attr('id', 'lefthand')
+		.attr('id', 'righthand');
 
 	// group that will contain y axis for left hand plot
 	d3.select('#lefthand').append('g').attr('id', 'yaxis')
@@ -118,250 +119,132 @@ function plot_it()  {
 
 function setup_vis(){
 	setup_buttons();
-	setup_pose(POSEKEYPOINT.NOSE, 0)
-	setup_righthand(HANDKEYPOINT.PALM, 0)
-	setup_lefthand(HANDKEYPOINT.PALM, 0)
 
-	d3.select('#selectButtonLeftHand').on('change', function(d){
-		var lefthandKeypoint = d3.select(this).property('value')
-		//console.log(lefthandKeypoint)
-		var threshold = thresholdScale(d3.select('#threshold').attr('y1'))
-		setup_lefthand(lefthandKeypoint, threshold)
-	})
-
-	d3.select('#selectButtonRightHand').on('change', function(d){
-		var righthandKeypoint = d3.select(this).property('value')
-		//console.log(righthandKeypoint)
-		var threshold = thresholdScale(d3.select('#threshold').attr('y1'))
-		setup_righthand(righthandKeypoint, threshold)
-	})
+	setup_plot('pose', POSEKEYPOINT.NECK, 0, 0, pose_data.length);
+	setup_plot('lefthand', HANDKEYPOINT.PALM, 0, 0, hand_left_data.length);
+	setup_plot('righthand', HANDKEYPOINT.PALM, 0, 0, hand_right_data.length);
 
 	d3.select('#selectButtonPose').on('change', function(d){
-		var poseKeypoint = d3.select(this).property('value')
-		//console.log(poseKeypoint)
-		var threshold = thresholdScale(d3.select('#threshold').attr('y1'))
-		//console.log(threshold)
-		setup_pose(poseKeypoint, threshold)
-	})
+		var poseKeypoint = d3.select(this).property('value');
+		var threshold = thresholdScale(d3.select('#threshold').attr('y1'));
+		setup_plot('pose', poseKeypoint, threshold, 0, pose_data.length);
+	});
+	d3.select('#selectButtonLeftHand').on('change', function(d){
+		var lefthandKeypoint = d3.select(this).property('value');
+		var threshold = thresholdScale(d3.select('#threshold').attr('y1'));
+		setup_plot('lefthand', lefthandKeypoint, threshold, 0, hand_left_data.length);
+	});
+	d3.select('#selectButtonRightHand').on('change', function(d){
+		var righthandKeypoint = d3.select(this).property('value');
+		var threshold = thresholdScale(d3.select('#threshold').attr('y1'));
+		setup_plot('righthand', righthandKeypoint, threshold, 0, hand_right_data.length);
+	});
 
-	filter_data()
+	filter_data();
+
+	brush_data();
 }
 
-function setup_pose(poseKeypoint, threshold){
+
+function setup_plot(plotType, keypointType, threshold, start, end){
+	var data;
+	if(plotType == 'pose'){
+		data = pose_data;
+	}
+	else if(plotType == 'lefthand'){
+		data = hand_left_data;
+	}
+	else if(plotType == 'righthand'){
+		data = hand_right_data;
+	}
+	else{
+		throw 'unknown plotType';
+	}
+
+	data = data.slice(start, end + 1);
+	//console.log(data);
+
+	// xScale and xAxis
 	var xScale = d3.scalePoint()
-					.domain(face_data.map(function(e, index){return index}))
+					.domain(data.map(function(e, index){return start + index;}))
 					.range([0, width])
-
-	var step = 100
+	var step = Math.ceil((end - start) / 15);
+	//console.log(step);
 	var xAxis = d3.axisBottom(xScale)
-					.tickValues(d3.range(0, face_data.length, step))
+					.tickValues(d3.range(start, end, step));
 
-	// yScale and yAxis for pose plot
-	var new_pose_data = pose_data.filter(function(d){
-		var c1 = d[3 * poseKeypoint + 1]
-		var c2 = d[3 * poseKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
+	// yScale and yAxis
+	var filtered_data = data.filter(function(d){
+		//console.log(3 * keypointType + 1);
+		var c1 = d[3 * keypointType + 1];
+		var c2 = d[3 * keypointType + 2];
+		return c1 >= threshold && c2 >= threshold;
 	})
 
-	var max = d3.max(new_pose_data, d => d[3 * poseKeypoint])
-	var min = d3.min(new_pose_data, d => d[3 * poseKeypoint])
+	var max = d3.max(filtered_data, d => d[3 * keypointType]);
+	var min = d3.min(filtered_data, d => d[3 * keypointType]);
 
-	var pose_yScale = d3.scaleLinear()
+	//console.log(filtered_data);
+
+	var yScale = d3.scaleLinear()
 					.domain([min, max])
-					.range([height, 0])
+					.range([height, 0]);
 
-	var pose_yAxis = d3.axisLeft(pose_yScale)
+	var yAxis = d3.axisLeft(yScale);
 
-	var poseSelection = d3.select('#pose')
-							.selectAll('.pose_plot')
-							.data(pose_data)
+	var selection = d3.select('#' + plotType)
+						.selectAll('.' + plotType + '_plot')
+						.data(data);
 
-	poseSelection.exit().remove()
+	selection.exit().remove()
 
-	poseSelection.enter()
-					.append('circle')
-					.attr('class', 'pose_plot')
-					.merge(poseSelection)
-					.attr('cx', function(d, index){return xScale(index)})
-					.transition()
-					.duration(800)
-					.attr('cy', function(d, index){
-						if(d.length == 0 || d[3 * poseKeypoint + 1] < threshold || d[3 * poseKeypoint + 2] < threshold){
-							//console.log('Index: ', index, ' has no data')
-							return height
-						}
-						else{
-							return pose_yScale(d[3 * poseKeypoint])
-						}
-					})
-					.attr('r', 1)
+	selection.enter()
+				.append('circle')
+				.attr('class', plotType + '_plot')
+				.merge(selection)
+				.attr('cx', function(d, index){return xScale(start + index);})
+				.transition()
+				.duration(800)
+				.attr('cy', function(d, index){
+					if(d.length == 0 || d[3 * keypointType + 1] < threshold || d[3 * keypointType + 2] < threshold){
+						//console.log('Index: ', index, ' has no data')
+						return height;
+					}
+					else{
+						return yScale(d[3 * keypointType]);
+					}
+				})
+				.attr('r', 1)
 
-	poseSelection.filter(function(d){
-						var c1 = d[3 * poseKeypoint + 1]
-						var c2 = d[3 * poseKeypoint + 2]
-						return c1 < threshold || c2 < threshold
-					})
-					.attr('opacity', 0)
-	poseSelection.filter(function(d){
-		var c1 = d[3 * poseKeypoint + 1]
-		var c2 = d[3 * poseKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
-	}).attr('opacity', 1)
-
-
-	d3.select('#pose').select('#xaxis')
-    	.call(xAxis)
-    	.attr('transform', 'translate(0,' + height + ')')
-
-    d3.select('#pose').select('#yaxis')
-		.call(pose_yAxis)
-
-}
-
-function setup_righthand(righthandKeypoint, threshold){
-	var xScale = d3.scalePoint()
-					.domain(face_data.map(function(e, index){return index}))
-					.range([0, width])
-
-	var step = 100
-	var xAxis = d3.axisBottom(xScale)
-					.tickValues(d3.range(0, hand_right_data.length, step))
-
-	// yScale and yAxis for right hand plot
-	var new_hand_right_data = hand_right_data.filter(function(d){
-		var c1 = d[3 * righthandKeypoint + 1]
-		var c2 = d[3 * righthandKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
-	})
-
-	var max = d3.max(new_hand_right_data, d => d[3 * righthandKeypoint])
-	var min = d3.min(new_hand_right_data, d => d[3 * righthandKeypoint])
-
-	//console.log(max, min)
-
-	var hand_right_yScale = d3.scaleLinear()
-					.domain([min, max])
-					.range([height, 0])
-
-	var hand_right_yAxis = d3.axisLeft(hand_right_yScale)
-
-	var righthandSelection = d3.select('#righthand')
-								.selectAll('.right_hand_plot')
-								.data(hand_right_data)
-
-	righthandSelection.exit().remove()
-
-	righthandSelection.enter()
-						.append('circle')
-						.attr('class', 'right_hand_plot')
-						.merge(righthandSelection)
-						.attr('cx', function(d, index){return xScale(index)})
-						.transition()
-						.duration(800)
-						.attr('cy', function(d, index){
-							if(d.length == 0 || d[3 * righthandKeypoint + 1] < threshold || d[3 * righthandKeypoint + 2] < threshold){
-								//console.log('Index: ', index, ' has no data')
-								return height
-							}
-							else{
-								return hand_right_yScale(d[3 * righthandKeypoint])
-							}
-						})
-						.attr('r', 1)
-
-	righthandSelection.filter(function(d){
-						var c1 = d[3 * righthandSelection + 1]
-						var c2 = d[3 * righthandSelection + 2]
-						return c1 < threshold || c2 < threshold
+	selection.filter(function(d){
+						var c1 = d[3 * keypointType + 1];
+						var c2 = d[3 * keypointType + 2];
+						return c1 < threshold || c2 < threshold;
 					})
 					.attr('opacity', 0)
-	righthandSelection.filter(function(d){
-		var c1 = d[3 * righthandKeypoint + 1]
-		var c2 = d[3 * righthandKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
-	}).attr('opacity', 1)
 
-	d3.select('#righthand').select('#xaxis')
+	selection.filter(function(d){
+		var c1 = d[3 * keypointType + 1];
+		var c2 = d[3 * keypointType + 2];
+		return c1 >= threshold && c2 >= threshold;
+	}).attr('opacity', 1);
+
+
+	d3.select('#' + plotType)
+		.select('#xaxis')
     	.call(xAxis)
-    	.attr('transform', 'translate(0,' + height + ')')
+    	.attr('transform', 'translate(0,' + height + ')');
 
-    d3.select('#righthand').select('#yaxis')
-		.call(hand_right_yAxis)
+    d3.select('#' + plotType)
+    	.select('#yaxis')
+		.call(yAxis);
 }
 
-function setup_lefthand(lefthandKeypoint, threshold){
-	var xScale = d3.scalePoint()
-					.domain(face_data.map(function(e, index){return index}))
-					.range([0, width])
-
-	var step = 100
-	var xAxis = d3.axisBottom(xScale)
-					.tickValues(d3.range(0, face_data.length, step))
-
-	var new_hand_left_data = hand_left_data.filter(function(d){
-		var c1 = d[3 * lefthandKeypoint + 1]
-		var c2 = d[3 * lefthandKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
-	})
-
-	var max = d3.max(new_hand_left_data, d => d[3 * lefthandKeypoint])
-	var min = d3.min(new_hand_left_data, d => d[3 * lefthandKeypoint])
-
-	var hand_left_yScale = d3.scaleLinear()
-							.domain([min, max])
-							.range([height, 0])
-
-	var hand_left_yAxis = d3.axisLeft(hand_left_yScale)
-
-	var lefthandSelection = d3.select('#lefthand')
-								.selectAll('.left_hand_plot')
-								.data(hand_left_data)
-
-	lefthandSelection.exit()
-					.remove()
-
-	lefthandSelection.enter()
-						.append('circle')
-						.attr('class', 'left_hand_plot')
-						.merge(lefthandSelection)
-						.attr('cx', function(d, index){return xScale(index)})
-						.transition()
-						.duration(800)
-						.attr('cy', function(d, index){
-							if(d.length == 0 || d[3 * lefthandKeypoint + 1] < threshold || d[3 * lefthandKeypoint + 2] < threshold){
-								//console.log('Index: ', index, ' has no data')
-								return height
-							}
-							else{
-								return hand_left_yScale(d[3 * lefthandKeypoint])
-							}
-						})
-						.attr('r', 1)
-
-	lefthandSelection.filter(function(d){
-						var c1 = d[3 * lefthandKeypoint + 1]
-						var c2 = d[3 * lefthandKeypoint + 2]
-						return c1 < threshold || c2 < threshold
-					})
-					.attr('opacity', 0)
-	lefthandSelection.filter(function(d){
-		var c1 = d[3 * lefthandKeypoint + 1]
-		var c2 = d[3 * lefthandKeypoint + 2]
-		return c1 >= threshold && c2 >= threshold
-	}).attr('opacity', 1)
-
-    d3.select('#lefthand').select('#xaxis')
-    	.call(xAxis)
-    	.attr('transform', 'translate(0,' + height + ')')
-
-    d3.select('#lefthand').select('#yaxis')
-		.call(hand_left_yAxis)
-}
 
 function setup_buttons(){
 	var hand = [];
 	for(var key in HANDKEYPOINT){
-		hand.push(key)
+		hand.push(key);
 	}
 	d3.select('#selectButtonLeftHand')
 		.selectAll('options')
@@ -369,7 +252,7 @@ function setup_buttons(){
 		.enter()
 		.append('option')
 		.text(function(d){return d})
-		.attr('value', function(d){return HANDKEYPOINT[d]})
+		.attr('value', function(d){return HANDKEYPOINT[d]});
 
 	
 	d3.select('#selectButtonRightHand')
@@ -378,12 +261,12 @@ function setup_buttons(){
 		.enter()
 		.append('option')
 		.text(function(d){return d})
-		.attr('value', function(d){return HANDKEYPOINT[d]})
+		.attr('value', function(d){return HANDKEYPOINT[d]});
 
 
-	var pose = []
+	var pose = [];
 	for(var key in POSEKEYPOINT){
-		pose.push(key)
+		pose.push(key);
 	}
 	d3.select('#selectButtonPose')
 		.selectAll('options')
@@ -391,36 +274,82 @@ function setup_buttons(){
 		.enter()
 		.append('option')
 		.text(function(d){return d})
-		.attr('value', function(d){return POSEKEYPOINT[d]})
+		.attr('value', function(d){return POSEKEYPOINT[d]});
 }
 
 var thresholdScale = d3.scaleLinear()
 							.domain([0, filter_height])
-							.range([1, 0])
+							.range([1, 0]);
 
 
 function filter_data(){
 	function update_plots(){
-		var threshold = thresholdScale(d3.select('#threshold').attr('y1'))
-		var poseKeypoint = d3.select('#selectButtonPose').property('value')
-		var righthandKeypoint = d3.select('#selectButtonRightHand').property('value')
-		var lefthandKeypoint = d3.select('#selectButtonLeftHand').property('value')
-		//console.log(poseKeypoint, righthandKeypoint, lefthandKeypoint)
-		setup_pose(poseKeypoint, threshold)
-		setup_righthand(righthandKeypoint, threshold)
-		setup_lefthand(lefthandKeypoint, threshold)
+		var threshold = thresholdScale(d3.select('#threshold').attr('y1'));
+		var poseKeypoint = d3.select('#selectButtonPose').property('value');
+		var righthandKeypoint = d3.select('#selectButtonRightHand').property('value');
+		var lefthandKeypoint = d3.select('#selectButtonLeftHand').property('value');
+
+		setup_plot('pose', poseKeypoint, threshold, 0, pose_data.length);
+		setup_plot('lefthand', lefthandKeypoint, threshold, 0, hand_left_data.length);
+		setup_plot('righthand', righthandKeypoint, threshold, 0, hand_right_data.length);
 	}
 
 
 	function dragged() {
-	    var y = d3.mouse(this)[1]
+	    var y = d3.mouse(this)[1];
 	    d3.select(this)
 	    	.attr('y1', y <= filter_height ? (y >= 0 ? y : 0) : filter_height)
-	    	.attr('y2', y <= filter_height ? (y >= 0 ? y : 0) : filter_height)
+	    	.attr('y2', y <= filter_height ? (y >= 0 ? y : 0) : filter_height);
 	}
 	
 	var drag = d3.drag().on('drag', dragged)
-						.on('end', update_plots)
+						.on('end', update_plots);
 
-	d3.select('#threshold').call(drag)
+	d3.select('#threshold').call(drag);
 }
+
+function brush_data(){
+	console.log("Entering brush_data()");
+	var threshold = thresholdScale(d3.select('#threshold').attr('y1'));
+	var poseKeypoint = d3.select('#selectButtonPose').property('value');
+	var righthandKeypoint = d3.select('#selectButtonRightHand').property('value');
+	var lefthandKeypoint = d3.select('#selectButtonLeftHand').property('value');
+	function zoom_plots(){
+		var interval = d3.event.selection;
+		var ticks = d3.select('#pose')
+						.select('#xaxis')
+						.selectAll('.tick')
+						.data();
+		console.log(width * ticks.length / 16, ticks[0], ticks[1]);
+
+		var xScale = d3.scalePoint()
+						.domain(ticks)
+						.range([0, width * ticks.length / 16]);
+
+		var start;
+		var end;
+		for(var i = 0; i < ticks.length - 1; ++i){
+			var cur = xScale(ticks[i]);
+			var next = xScale(ticks[i + 1]);
+			console.log(cur, next);
+			if(cur <= interval[0] && next >= interval[0]){
+				start = ticks[i + 1];
+			}
+			if(cur <= interval[1] && next >= interval[1]){
+				end = ticks[i];
+			}
+		}
+		console.log(start, end);
+		setup_plot('pose', poseKeypoint, threshold, start, end);
+		setup_plot('lefthand', lefthandKeypoint, threshold, start, end);
+		setup_plot('righthand', righthandKeypoint, threshold, start, end);
+	}
+
+	var brush = d3.brushX()
+					.extent([[0, 0], [1078, 18]])
+					.on('end', zoom_plots);
+
+	d3.select('#pose')
+		.select('#xaxis')
+		.call(brush);
+	}
